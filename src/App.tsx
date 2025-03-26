82f29f59c53744cbc04ce07fffb23ce279d14968
Imran Nazir <imran.nazir@yahoo.co.uk>
2025-03-26 17:07:31 +0000

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +28,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <section id="container">
+        <div className="left">left</div>
+        <div className="right">right</div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
